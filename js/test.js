b02886b48e2e6db5073a7b0109c8f050bf070f3b
SergeiKPI <homych_sergei@mail.ru>
2017-11-28 22:10:08 +0200

    first commit

---
+++ js/test.js
@@ -0,0 +1,30 @@
+window.addEventListener("load",function(){
+    
+    var arr = document.querySelector(".father");
+    
+    var _1 = arr.children[0];
+    var _2 = arr.children[1];
+    var _3 = arr.children[2];
+    var _4 = arr.children[3];
+    var _5 = arr.children[4];
+    var _6 = arr.children[5];
+
+    _1.style.left = "110px";
+    _1.style.top = "45px";
+    
+    _2.style.left = "237px";
+    _2.style.top = "45px";
+    
+    _3.style.left = "172px";
+    _3.style.top = "180px";
+    
+    _4.style.left = "600px";
+    _4.style.top = "220px";
+    
+    _5.style.left = "600px";
+    _5.style.top = "350px";
+    
+    _6.style.left = "595px";
+    _6.style.top = "455px";
+    
+});
